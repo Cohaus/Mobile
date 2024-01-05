afd043b1833657a01ed7059b0b93c116b03b564f
Tnalxmsk <alstnwkd990@naver.com>
2024-01-05 22:52:52 +0900

    feat: 프로젝트 세팅
    Bottom Navigation, Toolbar,
    home, profile, map 레이아웃 추가

---
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/home/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.solution.gdsc.ui.home
+
+import com.solution.gdsc.R
+import com.solution.gdsc.base.BaseFragment
+import com.solution.gdsc.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun setLayout() {
+    }
+
+}
