b3c14fff4ca82a5cb3aadf0d169ebdb42d15cc53
Tnalxmsk <alstnwkd990@naver.com>
2024-01-13 20:29:44 +0900

    feat: 체크리스트 화면 추가

---
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/home/CheckListFragment.kt
@@ -0,0 +1,11 @@
+package com.solution.gdsc.ui.home
+
+import com.solution.gdsc.R
+import com.solution.gdsc.base.BaseFragment
+import com.solution.gdsc.databinding.FragmentChecklistBinding
+
+class CheckListFragment : BaseFragment<FragmentChecklistBinding>(R.layout.fragment_checklist) {
+    override fun setLayout() {
+
+    }
+}
