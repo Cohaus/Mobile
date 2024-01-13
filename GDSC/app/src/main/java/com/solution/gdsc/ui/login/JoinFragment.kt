2f073418bf85887aab729eae334f3c215e1a43f0
Tnalxmsk <alstnwkd990@naver.com>
2024-01-13 20:19:26 +0900

    feat: 회원가입 화면구현

---
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/login/JoinFragment.kt
@@ -0,0 +1,11 @@
+package com.solution.gdsc.ui.login
+
+import com.solution.gdsc.R
+import com.solution.gdsc.base.BaseFragment
+import com.solution.gdsc.databinding.FragmentJoinBinding
+
+class JoinFragment : BaseFragment<FragmentJoinBinding>(R.layout.fragment_join) {
+    override fun setLayout() {
+
+    }
+}
--- GDSC/app/src/main/java/com/solution/gdsc/ui/login/LoginActivity.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/login/LoginActivity.kt
@@ -1,12 +1,14 @@
 package com.solution.gdsc.ui.login
 
+import android.content.Intent
 import com.solution.gdsc.R
 import com.solution.gdsc.base.BaseActivity
 import com.solution.gdsc.databinding.ActivityLoginBinding
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
     override fun setLayout() {
-
+        binding.btnJoin.setOnClickListener {
+        }
     }
 
 }
