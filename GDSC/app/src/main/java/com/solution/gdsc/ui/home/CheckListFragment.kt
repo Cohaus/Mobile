28fc9bdbd784f4a4ee72934fc47fe341fa316879
Tnalxmsk <alstnwkd990@naver.com>
2024-01-14 18:54:02 +0900

    feat: 안전수칙 체크 화면 구현

--- GDSC/app/src/main/java/com/solution/gdsc/ui/home/CheckListFragment.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/home/CheckListFragment.kt
@@ -1,14 +1,24 @@
 package com.solution.gdsc.ui.home
 
+import android.widget.CheckBox
 import androidx.navigation.fragment.findNavController
 import com.solution.gdsc.R
 import com.solution.gdsc.base.BaseFragment
 import com.solution.gdsc.databinding.FragmentChecklistBinding
+import com.solution.gdsc.ui.extensions.changeTextColor
 
 class CheckListFragment : BaseFragment<FragmentChecklistBinding>(R.layout.fragment_checklist) {
     override fun setLayout() {
-        binding.toolbarChecklist.setNavigationOnClickListener {
-            findNavController().navigateUp()
+        with(binding) {
+            cbCheckboxFirst.changeTextColor()
+            cbCheckboxSecond.changeTextColor()
+            cbCheckboxThird.changeTextColor()
+            cbCheckboxFourth.changeTextColor()
+            cbCheckboxFifth.changeTextColor()
+
+            toolbarChecklist.setNavigationOnClickListener {
+                findNavController().navigateUp()
+            }
         }
     }
 }
