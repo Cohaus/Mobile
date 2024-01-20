1e4ad5322a13ddd06d86775ba784d3d14ba6b333
Tnalxmsk <alstnwkd990@naver.com>
2024-01-21 01:54:22 +0900

    feat: 하단 모달 추가

--- GDSC/app/src/main/java/com/solution/gdsc/ui/home/CameraFragment.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/home/CameraFragment.kt
@@ -7,12 +7,12 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
-import androidx.exifinterface.media.ExifInterface
 import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import androidx.core.content.FileProvider
+import androidx.exifinterface.media.ExifInterface
 import androidx.navigation.fragment.findNavController
 import com.solution.gdsc.R
 import com.solution.gdsc.base.BaseFragment
