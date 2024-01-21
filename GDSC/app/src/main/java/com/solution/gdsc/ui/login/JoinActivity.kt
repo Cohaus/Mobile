164a4aeaefa9847bef9b42f9fb113ce01de8bd5f
Tnalxmsk <alstnwkd990@naver.com>
2024-01-21 18:04:35 +0900

    feat: 회원 가입 화면 확인 버튼 state 추가 (임시)

--- GDSC/app/src/main/java/com/solution/gdsc/ui/login/JoinActivity.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/login/JoinActivity.kt
@@ -1,13 +1,58 @@
 package com.solution.gdsc.ui.login
 
+import androidx.core.widget.doAfterTextChanged
 import com.solution.gdsc.R
 import com.solution.gdsc.base.BaseActivity
 import com.solution.gdsc.databinding.FragmentJoinBinding
 
 class JoinActivity : BaseActivity<FragmentJoinBinding>(R.layout.fragment_join) {
+    private var userId = ""
+    private var password = ""
+    private var userName = ""
+    private var userEmail = ""
+
+    private var isValidUserId = false
+    private var isValidPassword = false
+    private var isValidUserName = false
+    private var isValidUserEmail = false
+
     override fun setLayout() {
         binding.toolbarJoinMembership.setNavigationOnClickListener {
             finish()
         }
+        setTextInput()
+    }
+
+    private fun setTextInput() {
+        with(binding) {
+            etInputJoinId.doAfterTextChanged {
+                userId = it?.toString() ?: ""
+                isValidUserId = isValidInput(userId)
+                updateButtonEnableState()
+
+            }
+            etInputJoinPassword.doAfterTextChanged {
+                password = it?.toString() ?: ""
+                isValidPassword = isValidInput(password)
+                updateButtonEnableState()
+            }
+            etInputJoinName.doAfterTextChanged {
+                userName = it?.toString() ?: ""
+                isValidUserName = isValidInput(userName)
+                updateButtonEnableState()
+            }
+            etInputJoinEmail.doAfterTextChanged {
+                userEmail = it?.toString() ?: ""
+                isValidUserEmail = isValidInput(userEmail)
+                updateButtonEnableState()
+            }
+        }
+    }
+
+    private fun isValidInput(text: String): Boolean = text.isNotBlank()
+
+    private fun updateButtonEnableState() {
+        binding.btnJoinConfirm.isEnabled =
+            isValidUserId && isValidPassword && isValidUserName && isValidUserEmail
     }
 }
