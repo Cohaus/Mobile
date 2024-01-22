b7b33f1b285e9d9c90bd1f174879200583ad47bb
Tnalxmsk <alstnwkd990@naver.com>
2024-01-23 06:30:40 +0900

    feat: NetworkModule LoginOkHttpClient 분리

---
+++ GDSC/app/src/main/java/com/solution/gdsc/di/NetworkModule.kt
@@ -0,0 +1,58 @@
+package com.solution.gdsc.di
+
+import com.solution.gdsc.util.Utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class LoginInterceptorOkHttpClient
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class OtherInterceptorOkHttpClient
+
+    @LoginInterceptorOkHttpClient
+    @Singleton
+    @Provides
+    fun provideLoginOkHttpClient(loginInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loginInterceptor)
+            .build()
+    }
+
+    @LoginInterceptorOkHttpClient
+    @Singleton
+    @Provides
+    fun provideLoginRetrofit(
+        gsonConverterFactory: GsonConverterFactory,
+        @LoginInterceptorOkHttpClient client: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(): GsonConverterFactory = GsonConverterFactory.create()
+}
