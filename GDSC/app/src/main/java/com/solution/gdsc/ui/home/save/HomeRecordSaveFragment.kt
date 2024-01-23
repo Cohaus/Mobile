615eb86a708e4f673949a44fd1a15af11e1c5605
Tnalxmsk <alstnwkd990@naver.com>
2024-01-24 02:48:38 +0900

    feat: HomeRecordSaveFragment 추가
    EditText 값에 따른 Button Enabled 변경 기능 구현

---
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/home/save/HomeRecordSaveFragment.kt
@@ -0,0 +1,50 @@
+package com.solution.gdsc.ui.home.save
+
+import androidx.core.widget.doAfterTextChanged
+import androidx.navigation.fragment.findNavController
+import com.solution.gdsc.R
+import com.solution.gdsc.base.BaseFragment
+import com.solution.gdsc.databinding.FragmentHomeRecordSaveBinding
+
+class HomeRecordSaveFragment : BaseFragment<FragmentHomeRecordSaveBinding>(R.layout.fragment_home_record_save) {
+    private var inputTitle = ""
+    private var inputContent = ""
+
+    private var isValidTitle = false
+    private var isValidContent = false
+
+    override fun setLayout() {
+        setTextInput()
+        with(binding) {
+            toolbarRecordSave.setNavigationOnClickListener {
+                findNavController().navigateUp()
+            }
+            btnRecordSave.setOnClickListener {
+                //
+            }
+        }
+    }
+
+    private fun setTextInput() {
+        with(binding) {
+            etRecordInputTitle.doAfterTextChanged {
+                inputTitle = it?.toString() ?: ""
+                isValidTitle = isValidInput(inputTitle)
+                updateButtonEnableState()
+
+            }
+            etRecordInputContent.doAfterTextChanged {
+                inputContent = it?.toString() ?: ""
+                isValidContent = isValidInput(inputContent)
+                updateButtonEnableState()
+            }
+        }
+    }
+
+
+    private fun isValidInput(text: String): Boolean = text.isNotBlank()
+
+    private fun updateButtonEnableState() {
+        binding.btnRecordSave.isEnabled = isValidTitle && isValidContent
+    }
+}
