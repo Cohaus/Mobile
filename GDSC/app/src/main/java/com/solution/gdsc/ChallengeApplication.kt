7a43e743d2e63e247ed43e6546714ba7192e4b73
Tnalxmsk <alstnwkd990@naver.com>
2024-01-25 05:44:56 +0900

    refactor: DI 개선

--- GDSC/app/src/main/java/com/solution/gdsc/ChallengeApplication.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/ChallengeApplication.kt
@@ -1,12 +1,26 @@
 package com.solution.gdsc
 
 import android.app.Application
+import com.solution.gdsc.util.TokenManager
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class ChallengeApplication : Application() {
 
+    @Inject
+    lateinit var tokenManager: TokenManager
+
+    companion object {
+        private lateinit var application: ChallengeApplication
+
+        fun getInstance(): ChallengeApplication {
+            return application
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
+        application = this
     }
 }
--- GDSC/app/src/main/java/com/solution/gdsc/ui/login/LoginActivity.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/login/LoginActivity.kt
@@ -2,21 +2,17 @@ package com.solution.gdsc.ui.login
 
 import android.content.Intent
 import androidx.activity.viewModels
+import com.solution.gdsc.ChallengeApplication
 import com.solution.gdsc.R
 import com.solution.gdsc.base.BaseActivity
 import com.solution.gdsc.databinding.ActivityLoginBinding
 import com.solution.gdsc.ui.MainActivity
 import com.solution.gdsc.ui.login.viewmodel.LoginViewModel
-import com.solution.gdsc.util.TokenManager
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
     private val viewModel by viewModels<LoginViewModel>()
-
-    @Inject
-    lateinit var tokenManager: TokenManager
 
     override fun setLayout() {
         binding.btnJoin.setOnClickListener {
@@ -28,7 +24,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
     }
 
     private fun checkUserLogin() {
-        if (tokenManager.checkUserToken()) {
+        if (ChallengeApplication.getInstance().tokenManager.checkUserToken()) {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
