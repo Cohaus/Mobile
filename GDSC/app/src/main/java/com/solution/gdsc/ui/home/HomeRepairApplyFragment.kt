9e27f057eab5729233a84b0c389aa8da3cd63d40
Tnalxmsk <alstnwkd990@naver.com>
2024-01-28 14:44:18 +0900

    feat: 수리 신청 완료 후 홈 화면 이동 구현

--- GDSC/app/src/main/java/com/solution/gdsc/ui/home/HomeRepairApplyFragment.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/home/HomeRepairApplyFragment.kt
@@ -1,11 +1,15 @@
 package com.solution.gdsc.ui.home
 
+import androidx.navigation.fragment.findNavController
 import com.solution.gdsc.R
 import com.solution.gdsc.base.BaseFragment
 import com.solution.gdsc.databinding.FragmentHomeRepairApplyBinding
 
 class HomeRepairApplyFragment : BaseFragment<FragmentHomeRepairApplyBinding>(R.layout.fragment_home_repair_apply) {
     override fun setLayout() {
-
+        binding.btnRepairApplySave.setOnClickListener {
+            val action = HomeRepairApplyFragmentDirections.actionHomeRepairApplyToHome()
+            findNavController().navigate(action)
+        }
     }
 }
