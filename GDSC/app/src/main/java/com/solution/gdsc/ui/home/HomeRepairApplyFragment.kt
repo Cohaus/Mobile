fe8bda0c73323e657ecba84f5b0849b5fa2fccdc
Tnalxmsk <alstnwkd990@naver.com>
2024-01-28 14:20:56 +0900

    feat: 수리 신청 화면 구현

---
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/home/HomeRepairApplyFragment.kt
@@ -0,0 +1,11 @@
+package com.solution.gdsc.ui.home
+
+import com.solution.gdsc.R
+import com.solution.gdsc.base.BaseFragment
+import com.solution.gdsc.databinding.FragmentHomeRepairApplyBinding
+
+class HomeRepairApplyFragment : BaseFragment<FragmentHomeRepairApplyBinding>(R.layout.fragment_home_repair_apply) {
+    override fun setLayout() {
+
+    }
+}
