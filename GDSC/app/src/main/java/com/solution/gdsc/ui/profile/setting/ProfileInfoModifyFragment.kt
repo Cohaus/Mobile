e2a1a1b80e769443ac71320ebd2a5b5de06481dc
Tnalxmsk <alstnwkd990@naver.com>
2024-01-31 23:49:06 +0900

    feat: 회원정보수정 화면까지 UserInfoDto 전달 구현

--- GDSC/app/src/main/java/com/solution/gdsc/ui/profile/setting/ProfileInfoModifyFragment.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/profile/setting/ProfileInfoModifyFragment.kt
@@ -1,10 +1,17 @@
 package com.solution.gdsc.ui.profile.setting
 
+import androidx.navigation.fragment.navArgs
 import com.solution.gdsc.R
 import com.solution.gdsc.base.BaseFragment
 import com.solution.gdsc.databinding.FragmentProfileInfoModifyBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileInfoModifyFragment : BaseFragment<FragmentProfileInfoModifyBinding>(R.layout.fragment_profile_info_modify) {
+    private val args by navArgs<ProfileInfoModifyFragmentArgs>()
+
     override fun setLayout() {
+        val userInfo = args.userInfo
+        binding.userInfo = userInfo
     }
 }
