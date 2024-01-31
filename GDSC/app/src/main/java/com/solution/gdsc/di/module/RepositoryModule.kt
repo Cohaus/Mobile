e3b26d27f50b96eab7fd9dfc75a47716a7e2e71f
Tnalxmsk <alstnwkd990@naver.com>
2024-02-01 03:22:58 +0900

    chore: RepositoryModule HomeRepository 의존성 주입

--- GDSC/app/src/main/java/com/solution/gdsc/di/module/RepositoryModule.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/di/module/RepositoryModule.kt
@@ -1,9 +1,12 @@
 package com.solution.gdsc.di.module
 
-import com.solution.gdsc.data.source.LoginDataSource
-import com.solution.gdsc.data.source.LoginRepositoryImpl
-import com.solution.gdsc.data.source.UserMyPageDatasource
-import com.solution.gdsc.data.source.UserMyPageRepositoryImpl
+import com.solution.gdsc.data.source.home.HomeDataSource
+import com.solution.gdsc.data.source.home.HomeRepositoryImpl
+import com.solution.gdsc.data.source.login.LoginDataSource
+import com.solution.gdsc.data.source.login.LoginRepositoryImpl
+import com.solution.gdsc.data.source.profile.UserMyPageDatasource
+import com.solution.gdsc.data.source.profile.UserMyPageRepositoryImpl
+import com.solution.gdsc.domain.repository.HomeRepository
 import com.solution.gdsc.domain.repository.LoginRepository
 import com.solution.gdsc.domain.repository.UserMyPageRepository
 import dagger.Module
@@ -25,4 +28,9 @@ object RepositoryModule {
     @Provides
     fun provideUserMyPageRepository(userMyPageDatasource: UserMyPageDatasource): UserMyPageRepository =
         UserMyPageRepositoryImpl(userMyPageDatasource)
+
+    @Singleton
+    @Provides
+    fun provideHomeRepository(homeDataSource: HomeDataSource): HomeRepository =
+        HomeRepositoryImpl(homeDataSource)
 }
