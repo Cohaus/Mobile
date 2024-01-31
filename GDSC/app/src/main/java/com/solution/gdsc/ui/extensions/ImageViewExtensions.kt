d2872a60a159db729965a50783824e9e4ccde19d
Tnalxmsk <alstnwkd990@naver.com>
2024-02-01 02:01:15 +0900

    feat: Glide 이미지 로드 라이브러리 추가

---
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/extensions/ImageViewExtensions.kt
@@ -0,0 +1,19 @@
+package com.solution.gdsc.ui.extensions
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.solution.gdsc.R
+
+@BindingAdapter("imageUrl")
+fun ImageView.load(url: String?) {
+    if (!url.isNullOrEmpty()) {
+        Glide.with(this)
+            .load(url)
+            .placeholder(R.color.gray_300)
+            .error(R.drawable.home_image)
+            .into(this)
+    } else {
+        setImageResource(R.drawable.home_image)
+    }
+}
