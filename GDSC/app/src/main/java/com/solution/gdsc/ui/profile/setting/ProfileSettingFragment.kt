2bb88157b2290e439858f8db737adef96796974d
Tnalxmsk <alstnwkd990@naver.com>
2024-02-04 18:02:52 +0900

    feat: 설정 화면 UI 변경

--- GDSC/app/src/main/java/com/solution/gdsc/ui/profile/setting/ProfileSettingFragment.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/ui/profile/setting/ProfileSettingFragment.kt
@@ -6,6 +6,7 @@ import com.solution.gdsc.R
 import com.solution.gdsc.base.BaseFragment
 import com.solution.gdsc.databinding.FragmentProfileSettingBinding
 import com.solution.gdsc.ui.common.DialogCategory
+import com.solution.gdsc.util.Utils
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,6 +14,7 @@ class ProfileSettingFragment : BaseFragment<FragmentProfileSettingBinding>(R.lay
     private val args by navArgs<ProfileSettingFragmentArgs>()
 
     override fun setLayout() {
+        checkUserType()
         with(binding) {
             toolbarSetting.setNavigationOnClickListener {
                 findNavController().navigateUp()
@@ -35,6 +37,12 @@ class ProfileSettingFragment : BaseFragment<FragmentProfileSettingBinding>(R.lay
                 val action = ProfileSettingFragmentDirections.actionProfileSettingToSettingVolunteerRegistration()
                 findNavController().navigate(action)
             }
+        }
+    }
+
+    private fun checkUserType() {
+        if (args.userInfo.volunteerType != null) {
+            binding.tvVolunteerRegistration.text = Utils.VOLUNTEER_UPDATE
         }
     }
 }
--- GDSC/app/src/main/java/com/solution/gdsc/util/Utils.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/util/Utils.kt
@@ -2,4 +2,6 @@ package com.solution.gdsc.util
 
 object Utils {
     const val BASE_URL = "http://35.216.105.128:8080/"
+    const val GEOCODING_URL = "https://maps.googleapis.com/maps/api/place/details/json"
+    const val VOLUNTEER_UPDATE = "봉사자 정보 수정"
 }
