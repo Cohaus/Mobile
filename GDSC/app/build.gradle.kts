e9e0de2973e99af9629c1c7cec1a3a7a2f35f9d8
Tnalxmsk <alstnwkd990@naver.com>
2024-02-16 17:08:51 +0900

    build: splash screen 의존성 추가

--- GDSC/app/build.gradle.kts
+++ GDSC/app/build.gradle.kts
@@ -79,10 +79,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
+    implementation("androidx.core:core-splashscreen:1.0.1")
     // navigation graph
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     // Retrofit2
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
