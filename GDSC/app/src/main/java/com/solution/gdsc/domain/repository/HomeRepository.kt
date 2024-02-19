52af0830282745033e16e11cdce248e06099dfec
Tnalxmsk <alstnwkd990@naver.com>
2024-02-19 16:58:11 +0900

    chore: 줄바꿈 개선

--- GDSC/app/src/main/java/com/solution/gdsc/domain/repository/HomeRepository.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/domain/repository/HomeRepository.kt
@@ -5,8 +5,10 @@ import com.solution.gdsc.domain.model.response.RepairIdResponse
 import kotlinx.coroutines.flow.Flow
 
 interface HomeRepository {
-    suspend fun saveRecord(title: String, detail: String, grade: String, category: String,
-                           imageFilePath: String): Flow<DefaultResponse>
+    suspend fun saveRecord(
+        title: String, detail: String, grade: String,
+        category: String, imageFilePath: String): Flow<DefaultResponse>
+
     suspend fun postRepairBasicRecord(
         title: String, detail: String, category: String,
         placeId: String, address: String, district: String,
