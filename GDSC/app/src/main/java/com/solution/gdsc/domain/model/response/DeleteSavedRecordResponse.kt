44558e57c8a1d19cfdb2963ca25c8002ee9cf122
Tnalxmsk <alstnwkd990@naver.com>
2024-03-05 23:33:11 +0900

    refactor: data class 기본값 지정

--- GDSC/app/src/main/java/com/solution/gdsc/domain/model/response/DeleteSavedRecordResponse.kt
+++ GDSC/app/src/main/java/com/solution/gdsc/domain/model/response/DeleteSavedRecordResponse.kt
@@ -1,7 +1,7 @@
 package com.solution.gdsc.domain.model.response
 
 data class DeleteSavedRecordResponse(
-    val status: Int,
-    val message: String,
-    val data: Long
+    val status: Int = 0,
+    val message: String = "",
+    val data: Long = 0
 )
